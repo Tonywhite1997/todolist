d0f52212a8b9ca74932386d1c16657b951378438
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2022-06-16 17:04:11 +0100

    worked on the task lists functions. write the js code for the app

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,9 @@
+//selecting sliding navBar elements from the DOM.
 const slideInBtn = document.querySelector("header .fa-bars");
 const slideOutBtn = document.querySelector("header .fa-times");
 const navBar = document.querySelector(".header__nav")
 
-
+//navBar events listeners.
 slideInBtn.addEventListener("click", ()=>{
     navBar.classList.add("slide-in")
     slideInBtn.style.display = "none"
@@ -14,3 +15,147 @@ slideOutBtn.addEventListener("click", ()=>{
     slideInBtn.style.display = "flex"
     slideOutBtn.style.display = "none"
 })
+
+//writing the actions for my todolist app.
+
+const inputField = document.querySelector(".main .main__add__input__field")
+const taskList = document.querySelector(".main .main__task__lists")
+const addBtn = document.querySelector(".main .main__add__input__button")
+
+//saving user task to local storage
+
+let today = new Date();
+let date = `${today.getDate()}/${today.getMonth()}/${today.getFullYear()}`
+
+let userTask = {
+    date: "",
+    task: "",
+    completedColor: "",
+    completedStyle: "",
+    completedDate: ""
+}
+function saveUserTasks(){
+    let inputText = inputField.value
+    userTask = {
+        date,
+        task: inputText,
+        completedColor: "",
+        completedStyle: "",
+        completedDate: ""
+    }
+    if(!inputText || inputText[0] === " "){
+        return alert("Not valid")
+    }
+
+    let oldTasks = JSON.parse(localStorage.getItem("task")) || []
+    oldTasks.push(userTask)
+    localStorage.setItem("task", JSON.stringify(oldTasks))
+    location.reload()
+}
+
+//writing a function to create dviv for each task i the local storage and showing it to the user
+function showTasks(){
+    let oldTasks = JSON.parse(localStorage.getItem("task")) || []
+    for(let task of oldTasks){
+        let taskDiv = document.createElement("div")
+        taskDiv.innerHTML = `
+        <div class = "task" style="background-color:${task.completedColor}">
+            <p class="task__date">Created on:${task.date}</p>
+            <p class="task__task" style="text-decoration:${task.completedStyle}">${task.task}</p>
+            <button class="task__complete">Mark Complete</button>
+            
+            <i class="fas fa-times task__exit"></i>
+            <p class="main__task__lists__complete__message">Completed</p>
+        </div>
+        `
+
+        taskList.appendChild(taskDiv)
+    }
+
+}
+showTasks()
+
+//function delete task from the list
+
+function deleteTask(index){
+    let oldTasks = JSON.parse(localStorage.getItem("task")) || []
+    let unMatched = oldTasks.filter((task, i)=>{
+        return index !== i
+    })
+
+    if(oldTasks.length === 1){
+        localStorage.removeItem("task");
+        location.reload()
+    }
+
+    if(unMatched.length){
+        localStorage.setItem("task", JSON.stringify(unMatched))
+        location.reload()
+    }
+}
+
+//adding event listener to add task button
+addBtn.addEventListener("click", saveUserTasks)
+
+//adding eveent listener to delete button 
+const deleteBtns = document.querySelectorAll(".main__task__lists .task__exit")
+
+deleteBtns.forEach((deleteBtn, index)=>{
+    deleteBtn.addEventListener("click", ()=>{
+        deleteTask(index)
+    })
+})
+
+//writing a function to mark a task as complete
+const tasks = document.querySelectorAll(".task__task")
+function markComplete(index){
+    let oldTasks = JSON.parse(localStorage.getItem("task")) || []
+    let matched = oldTasks.find((data, i)=>{
+        return index === i
+    })
+
+    if(matched.completedDate !== ""){
+        return
+    }
+
+    let newArray = oldTasks.filter((data, i)=>{
+        return index !== i
+    })
+    if(matched){
+        userTask = {
+            date: matched.date,
+            task: matched.task,
+            completedColor: "lightgreen",
+            completedStyle: "line-through",
+            completedDate: date
+        }
+        newArray.push(userTask)
+        localStorage.setItem("task", JSON.stringify(newArray))
+
+        location.reload()
+    }
+}
+
+//adding event listener to mark complete button
+const markCompleteBtns = document.querySelectorAll(".task__complete")
+
+markCompleteBtns.forEach((markCompleteBtn, index)=>{
+    
+    markCompleteBtn.addEventListener("click", ()=>{
+        markComplete(index)
+    })
+})
+
+let completeMessage = document.querySelector(".main__task__lists__complete__message")
+markCompleteBtns.forEach((button, index)=>{
+    let oldTasks = JSON.parse(localStorage.getItem("task")) || []
+        if(oldTasks[index].completedDate !== ""){
+            button.innerText = `Completed on ${oldTasks[index].completedDate}`;
+            button.style.backgroundColor = "green"
+            
+        }
+})
+
+
+
+
